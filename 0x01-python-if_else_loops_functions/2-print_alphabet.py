600169d5513d654467f9c9798a5da4fa8fdd6d51
edidiongapostle <edidiongapostle@gmail.com>
2023-08-09 19:12:07 +0100

    tasks updated

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,6 @@
-#!/usr/bin/python3
-for char in range(ord('a'), ord('z') + 1):
-    print('{}'.format(chr(char)), end="")
+#!usr/bin/python3
+# 2-print_alphabet.py
+
+"""print the alphabet in lowecase, not followed by a new line."""
+for letter in range(97, 123):
+    print("{}".format(char(letter)), end="")
