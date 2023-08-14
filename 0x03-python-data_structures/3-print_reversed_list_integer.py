be3d5a94522fbb2d0ae5a026ad4cbc13f2145449
edidiongapostle <edidiongapostle@gmail.com>
2023-08-14 16:07:18 -0700

    data structures in python

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    __import__('3-print_reversed_list_integer').print_reversed_list_integer
+
+
+my_list = [1, 2, 3]
+print_reversed_list_integer(my_list)
