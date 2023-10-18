09d570a26f7e2480e23b64a7051e1c755f3e7124
edidiongapostle <edidiongapostle@gmail.com>
2023-10-18 02:50:09 +0100

    a script that creates the database hbtn_0c_0 in your MySQL server

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,5 +1,4 @@
--- a script that creates the database hbtn_0c_0
--- in your MySQL SERVER.
+-- a script that creates the database hbtn_0c_0 in your MySQL SERVER.
 
 CREATE DATABASE IF NOT EXISTS 'hbtn_0c_0';
 
